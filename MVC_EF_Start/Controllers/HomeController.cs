395e40dac386278b83d9ae3f0076739c24ad2fe1
rhthorat <Pravin@LAPTOP-1FP771V6>
2021-12-05 04:27:12 -0500

    resolved merge conflict

--- MVC_EF_Start/Controllers/HomeController.cs
+++ MVC_EF_Start/Controllers/HomeController.cs
@@ -35,55 +35,58 @@ namespace MVC_EF_Start.Controllers
             string[] committeeIDs = { "C00555748", "C00239533", "C00358796", "C00724070", "C00429613", "C00163121", "C00195065" };
             string[] candidateIDs = { "P40006033", "P00011569", "P40002172", "S2CO00175", "H0UT03227" };
 
-            /* foreach (string committeID in committeeIDs)
-             {
-                 httpClient = new HttpClient();
-                 httpClient.DefaultRequestHeaders.Accept.Clear();
-                 httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
-                 httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            foreach (string committeID in committeeIDs)
+            {
+                httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-                 string FinancialData_API_PATH = BASE_URL + "/committee/" + committeID + "/totals/?sort_hide_null=false&sort_nulls_last=false&page=1&api_key="
-                   + API_KEY + "&per_page=20&sort_null_only=false&sort=-cycle";
+                string FinancialData_API_PATH = BASE_URL + "/committee/" + committeID + "/totals/?sort_hide_null=false&sort_nulls_last=false&page=1&api_key="
+                  + API_KEY + "&per_page=20&sort_null_only=false&sort=-cycle";
 
-                 string financialData = null;
+                string financialData = null;
 
-                 httpClient.BaseAddress = new Uri(FinancialData_API_PATH);
-                 try
-                 {
-                     HttpResponseMessage response = httpClient.GetAsync(FinancialData_API_PATH)
-                                                         .GetAwaiter().GetResult();
+                httpClient.BaseAddress = new Uri(FinancialData_API_PATH);
+                try
+                {
+                    HttpResponseMessage response = httpClient.GetAsync(FinancialData_API_PATH)
+                                                        .GetAwaiter().GetResult();
 
-                     if (response.IsSuccessStatusCode)
-                     {
-                         financialData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-                     }
+                    if (response.IsSuccessStatusCode)
+                    {
+                        financialData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    }
 
-                     JObject parsedResponse = JObject.Parse(financialData);
-                     JArray fData = (JArray)parsedResponse["results"];
+                    JObject parsedResponse = JObject.Parse(financialData);
+                    JArray fData = (JArray)parsedResponse["results"];
 
-                     Result report = new Result();
-                     report.committee_id = (string)parsedResponse["results"][0]["committee_id"];
-                     report.cycle = (int)parsedResponse["results"][0]["cycle"];
-                     report.committee_state = (string)parsedResponse["results"][0]["committee_state"];
-                     report.committee_name = (string)parsedResponse["results"][0]["committee_name"];
-                     report.cash_on_hand_beginning_period = (float)parsedResponse["results"][0]["cash_on_hand_beginning_period"];
-                     report.net_contributions = (float)parsedResponse["results"][0]["net_contributions"];
-                     report.all_loans_received = (float)parsedResponse["results"][0]["all_loans_received"];
-                     report.net_operating_expenditures = (float)parsedResponse["results"][0]["net_operating_expenditures"];
-                     report.disbursements = (float)parsedResponse["results"][0]["disbursements"];
-                     report.last_cash_on_hand_end_period = (float)parsedResponse["results"][0]["last_cash_on_hand_end_period"];
-                     report.treasurer_name = (string)parsedResponse["results"][0]["treasurer_name"];
+                    Result report = new Result();
+                    report.committee_id = (string)parsedResponse["results"][0]["committee_id"];
+                    report.cycle = (int)parsedResponse["results"][0]["cycle"];
+                    report.committee_state = (string)parsedResponse["results"][0]["committee_state"];
+                    report.committee_name = (string)parsedResponse["results"][0]["committee_name"];
+                    report.cash_on_hand_beginning_period = (float)parsedResponse["results"][0]["cash_on_hand_beginning_period"];
+                    report.net_contributions = (float)parsedResponse["results"][0]["net_contributions"];
+                    report.all_loans_received = (float)parsedResponse["results"][0]["all_loans_received"];
+                    report.net_operating_expenditures = (float)parsedResponse["results"][0]["net_operating_expenditures"];
+                    report.disbursements = (float)parsedResponse["results"][0]["disbursements"];
+                    report.last_cash_on_hand_end_period = (float)parsedResponse["results"][0]["last_cash_on_hand_end_period"];
+                    report.treasurer_name = (string)parsedResponse["results"][0]["treasurer_name"];
 
-                     dbContext.results.Add(report);
+                    if (dbContext.results.Where(r => r.committee_id == committeID).ToList().Count == 0)
+                    {
+                        dbContext.results.Add(report);
+                    }
 
-                 }
-                 catch (Exception e)
-                 {
-                     // This is a useful place to insert a breakpoint and observe the error message
-                     Console.WriteLine(e.Message);
-                 }
-             }
-             dbContext.SaveChanges();*/
+                }
+                catch (Exception e)
+                {
+                    // This is a useful place to insert a breakpoint and observe the error message
+                    Console.WriteLine(e.Message);
+                }
+            }
+            dbContext.SaveChanges();
 
             //Fetching data from Filing API
             foreach (string candidateID in candidateIDs)
@@ -120,9 +123,10 @@ namespace MVC_EF_Start.Controllers
                     filingReport.means_filed = (string)parsedResponse["results"][0]["means_filed"];
                     filingReport.cycle = (int)parsedResponse["results"][0]["cycle"];
                     filingReport.pdf_url = (string)parsedResponse["results"][0]["pdf_url"];
-
-                    dbContext.filings.Add(filingReport);
-
+                    if (dbContext.filings.Where(r => r.candidate_id == candidateID).ToList().Count == 0)
+                    {
+                        dbContext.filings.Add(filingReport);
+                    }
                 }
                 catch (Exception e)
                 {
